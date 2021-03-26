071f4e9deb36c255f8b8d23fb3f505b0f81ea875
Aadiyta Vyas <74606669+AV7coder@users.noreply.github.com>
2021-03-26 18:12:27 +0530

    Add files via upload

---
+++ ColoredText.py
@@ -0,0 +1,10 @@
+import colorama
+import time
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.YELLOW + 'AV7CODER')
+print(Back.CYAN + 'AV7CODER')
+print(Style.BRIGHT + "BRIGHT")
+print(Fore.RED + Back.GREEN + "AV7CODER")
+time.sleep(100)
